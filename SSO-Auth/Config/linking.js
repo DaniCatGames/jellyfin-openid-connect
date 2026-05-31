0b9123baf118dcfcdf78517bcacb5ce63e4197a0
Eddy Moulton <8491021+eddymoulton@users.noreply.github.com>
2026-05-31 21:31:13 +1000

    Remove SAML
    Reducing surface area of this plugin by focusing on OIDC only

--- SSO-Auth/Config/linking.js
+++ SSO-Auth/Config/linking.js
@@ -2,12 +2,9 @@ const ssoConfigLinking = {
   pluginUniqueId: "ef25b893-ba73-4444-928c-01cd0e695ad4",
   loadProviders: (view) => {
     const provider_list_id = "sso-provider-list";
-    const provider_list_saml_id = `${provider_list_id}-saml`;
     const provider_list_oid_id = `${provider_list_id}-oid`;
 
-    const provider_list_saml = view.querySelector(`#${provider_list_saml_id}`);
     const provider_list_oid = view.querySelector(`#${provider_list_oid_id}`);
-    provider_list_saml.innerHTML = "";
     provider_list_oid.innerHTML = "";
 
     fetch(new Request(ApiClient.getUrl("sso/OID/GetNames"))).then((resp) => {
@@ -16,15 +13,6 @@ const ssoConfigLinking = {
           provider_list_oid,
           config_names,
           "oid",
-        );
-      });
-    });
-    fetch(new Request(ApiClient.getUrl("sso/SAML/GetNames"))).then((resp) => {
-      resp.json().then((config_names) => {
-        ssoConfigLinking.loadProviderList(
-          provider_list_saml,
-          config_names,
-          "saml",
         );
       });
     });
